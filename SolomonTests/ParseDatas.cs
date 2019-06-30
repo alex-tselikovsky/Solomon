8077136854fb5aa8b5dbf657729f244b90e1b386
flipworld <tmu87@mail.ru>
2019-06-30 11:18:10 -0700

    Create app

---
+++ SolomonTests/ParseDatas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace SolomonTests {
+    [TestClass]
+    public class ParseDatas {
+        [TestMethod]
+        public void ParseSgg()
+        {
+            var jsonSerializer = new JsonSerializer();
+
+            var path = @"C:\Users\Mikhail\source\repos\Solomon\SolomonTests\appeals_stavropol_with_comments.json";
+
+            //var result = jsonSerializer.Deserialize<>();
+        }
+    }
+}
---
+++ SolomonTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SolomonTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SolomonTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("24ebdc8c-3b72-41cd-a4b7-b4cad6743280")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
